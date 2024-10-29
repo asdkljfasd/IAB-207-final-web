1322661b655cbd81b62e511185be8ded642443cd
LukYuet2042911$ <brianchiu74@gmail.com>
2024-10-29 17:52:13 +1000

    installations

--- __init__.py
+++ __init__.py
@@ -2,3 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
+
+db = SQLAlchemy()
+
+def creat_app():
+    app=Flask(__name__)
+    app.debug=True
+    app.secret_key='testing123'
+    
+    #db config for app
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///RythmRally.db'
+    
+    # initialise database with Flask
+    db.init_app(app)
+    
+    bootstrap=Bootstrap(app)
+    
