bae2373ed72acfda0de3dfcf83f308a8077a7922
LukYuet2042911$ <brianchiu74@gmail.com>
2024-10-31 03:46:38 +1000

    x

--- RhythmRally/__init__.py
+++ RhythmRally/__init__.py
@@ -10,12 +10,14 @@ def create_app():
     app = Flask(__name__)
 
     Bootstrap5(app)
-    app.secret_key = 'somerandomvalue'
+    app.secret_key = 'testing123'
     # Initialize SQLAlchemy with the Flask app
 
 
     # Configue and initialise DB
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rhythm_rally.sqlite'
     db.init_app(app)
+    
     from .models import User, Event, Ticket, TicketStatus, Purchase, Review
+    
     return app
