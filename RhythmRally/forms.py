e2a13c06bcb084cbe35404e4cedbd018e9483c7c
ityuwoifenlbk <mpulman123@gmail.com>
2024-10-31 21:50:29 +1000

    Update forms.py
    eventform

--- RhythmRally/forms.py
+++ RhythmRally/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, InputRequired
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 #User login
@@ -23,3 +23,13 @@ class RegisterForm(FlaskForm):
     
     # Submit button
     submit = SubmitField("Register")
+
+class EventForm(FlaskForm):
+    eventname = StringField('Event Name', validators= [InputRequired("Please Enter the Event Name")])
+    artistname = StringField('Artist Name', validators= [InputRequired("Please Enter the Artist Name")])
+    description = TextAreaField('Description', validators= [InputRequired("Please Enter a Description")])
+    image = StringField('Event Image', validators= [InputRequired("Please Provide an Image")])
+    date = DateTimeField('Event Date', validators= [InputRequired("Please Enter the Date of the Event")])
+    start_time = DateTimeField('Start Time', validators= [InputRequired("Please Enter a Start Time")])
+    end_time = DateTimeField('End Time', validators= [InputRequired("Please Enter an End Time")])
+    ticket_price = InputRequired('Ticket Price', validators= [InputRequired("Please Enter a Ticket Price")])
