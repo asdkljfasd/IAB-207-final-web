e9d3c393e217807f127c4af9aa7a722e3b0bce26
kaynemishima <kcapuno2@gmail.com>
2024-10-31 21:46:59 +1000

    created register and login forms

--- RhythmRally/forms.py
+++ RhythmRally/forms.py
@@ -1,15 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, DateTimeField, IntegerField, FloatField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
 
-# form for user registration
-class RegistrationForm(FlaskForm):
-    first_name = StringField("First Name", validators=[DataRequired(), Length(max=50)])
-    last_name = StringField("Last Name", validators=[DataRequired(), Length(max=50)])
-    username = StringField("Username", validators=[DataRequired(), Length(min=4, max=25)])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
-    contact_number = StringField("Contact Number", validators=[DataRequired(), Length(max=15)])
-    street_address = StringField("Street Address", validators=[DataRequired(), Length(max=100)])
+#User login
+class LoginForm(FlaskForm):
+    user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[InputRequired('Enter user password')])
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    first_name = StringField("First Name", validators=[InputRequired()])
+    surname = StringField("Surname", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    password = PasswordField("Password", validators=[
+        InputRequired(),
+        EqualTo('confirm', message="Passwords should match")
+    ])
+    confirm = PasswordField("Confirm Password")
+    contact_number = StringField("Contact Number", validators=[InputRequired()])
+    street_address = StringField("Street Address", validators=[InputRequired()])
+    
+    # Submit button
     submit = SubmitField("Register")
