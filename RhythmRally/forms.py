e270c20c1832ee936d649e079e43fa6217e0d155
LukYuet2042911$ <brianchiu74@gmail.com>
2024-10-31 22:12:18 +1000

    fixed ticket_price string field

--- RhythmRally/forms.py
+++ RhythmRally/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, DateTimeField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, InputRequired
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
@@ -32,4 +32,4 @@ class EventForm(FlaskForm):
     date = DateTimeField('Event Date', validators= [InputRequired("Please Enter the Date of the Event")])
     start_time = DateTimeField('Start Time', validators= [InputRequired("Please Enter a Start Time")])
     end_time = DateTimeField('End Time', validators= [InputRequired("Please Enter an End Time")])
-    ticket_price = InputRequired('Ticket Price', validators= [InputRequired("Please Enter a Ticket Price")])
+    ticket_price = StringField('Ticket Price', validators= [InputRequired("Please Enter a Ticket Price")])
